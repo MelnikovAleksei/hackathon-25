0345e43e360a2d07c4cf25ef2b7c687158fc2819
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-30 10:39:38 +0300

    feat: добавить информацию о том, что в избранном нет стихотворений

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -15,6 +15,8 @@ export const Favorites = () => {
 
   const [modalIsOpen, setModalIsOpen] = React.useState(false);
 
+  const [modalArticleIsOpen, setModalArticleIsOpen] = React.useState(false);
+
   const [currentPoem, setCurrentPoem] = React.useState(null);
 
   const [favoritePoems, setFavoritePoems] = React.useState([]);
@@ -24,9 +26,19 @@ export const Favorites = () => {
     setModalIsOpen(true);
   }
 
+  const onOpenModalArticle = () => {
+    document.body.style.overflow = 'hidden';
+    setModalArticleIsOpen(true);
+  }
+
   const onCloseModal = () => {
     document.body.style.overflow = 'auto';
     setModalIsOpen(false);
+  }
+
+  const onCloseModalArticle = () => {
+    document.body.style.overflow = 'auto';
+    setModalArticleIsOpen(false);
   }
 
   const setPoems = () => {
@@ -73,17 +85,96 @@ export const Favorites = () => {
           Назад
         </Link>
       </div>
+      {favoritePoems.length === 0 &&
+          <>
+            <h3
+              className="main__title"
+            >
+              В избранном стихотворений нет
+            </h3>
+            <p
+              className="main__subtitle"
+            >
+              Как и тех слов, из которых состоит первое
+              четверостишие стихотворения Льюиса Кэррола "Бармаглот"
+            </p>
+            <article
+              className="article"
+            >
+              <p
+                className="article__text"
+              >
+                {'Варкалось. Хливкие шорьки \nПырялись по наве, \nИ хрюкотали зелюки, \nКак мюмзики в мове. \n'}
+              </p>
+              <p
+                className="article__text-info"
+              >
+                Русский перевод <a
+                                  className="article__link"
+                                  href="https://ru.wikipedia.org/wiki/%D0%9E%D1%80%D0%BB%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F,_%D0%94%D0%B8%D0%BD%D0%B0_%D0%93%D1%80%D0%B8%D0%B3%D0%BE%D1%80%D1%8C%D0%B5%D0%B2%D0%BD%D0%B0"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  Дины Орловской
+                                </a>
+              </p>
+            </article>
+            <button
+              className="article__button-open"
+              type="button"
+              onClick={onOpenModalArticle}
+            >
+              Прочитать на английском
+            </button>
+          </>}
       <ul
         className="main__list"
       >
         {favoritePoems && favoritePoems.map(poem => (
-          <PoemCard
-            key={poem.id}
-            poemData={poem}
-            handleClickOptions={handleClickOptions}
-          />
-        ))}
+            <PoemCard
+              key={poem.id}
+              poemData={poem}
+              handleClickOptions={handleClickOptions}
+            />
+          ))}
       </ul>
+      <Modal
+        className="modal"
+        overlayClassName="modal__overlay"
+        isOpen={modalArticleIsOpen}
+        onRequestClose={onCloseModalArticle}
+        shouldCloseOnOverlayClick={true}
+      >
+        <article
+          className="article"
+        >
+          <p
+            lang="en"
+            className="article__text"
+          >
+            {'Twas brillig, and the slithy toves \nDid gyre and gimble in the wabe; \nAll mimsy were the borogoves, \nAnd the mome raths outgrabe.'}
+          </p>
+          <p
+            lang="en"
+            className="article__text-info"
+          >
+            Jabberwocky
+          </p>
+          <p
+            lang="en"
+            className="article__text-info"
+          >
+            Lewis Carroll 1871
+          </p>
+        </article>
+        <button
+          className="modal__button-close"
+          type="button"
+          onClick={onCloseModalArticle}
+        >
+          Закрыть
+        </button>
+      </Modal>
       <Modal
         className="modal-options"
         overlayClassName="modal-options__overlay"
